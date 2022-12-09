3a1e01f120d9edfe101947d3ea7c5eb72c64705c
Rohit Kumar <rohit.rew@gmail.com>
2022-12-09 09:15:30 +0530

    project created with xcode

---
+++ main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  doublyLinkList
+//
+//  Created by Rohit Kumar on 09/12/22.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
